b1b736f1cc82cec13f70eaca64b3d9cf1753a40d
chaesc1223 <chaesc1223@naver.com>
2023-01-06 16:48:35 +0900

    Day03_0106

--- Day3_0106.playground/Contents.swift
+++ Day3_0106.playground/Contents.swift
@@ -23,5 +23,22 @@ let yourGrade = Grade.second.rawValue
 print("yourGrade : \(yourGrade )")
 
 enum SchoolDetail {
+    case elementary(name: String)
+    case middle(name: String)
+    case high(name: String)
     
+    func getName() -> String{
+        switch self{
+        case .elementary(let name):
+            return name
+        case let .middle(name):
+            return name
+        case .high(let name):
+            return name
+        }
+    }
 }
+
+let yourMiddleSchoolName = SchoolDetail.middle(name: "채정훈중학교")
+
+print("yourMiddleSchoolName: \(yourMiddleSchoolName.getName())")
