caf08af61f95feaa6aeca7cb0daa545b56fa2ed1
chaesc1223 <chaesc1223@naver.com>
2023-01-10 18:05:28 +0900

    Day08_0110

---
+++ Day08_0110.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//함수,메소드 정의
+func myFunc(name: String) -> String{
+    return "Hi : \(name) 입니다"
+}
+
+//함수, 메서드를 호출한다.call
+myFunc(name: "Chae")
+
+//함수,메소드 정의
+func myFuncSecond(hmm name: String) -> String{
+    return "Hi : \(name) 입니다"
+}
+
+myFuncSecond(hmm: "aasdasd")
+
+
+//함수,메소드 정의
+func myFuncThird(_ name: String) -> String{
+    return "Hi : \(name) 입니다"
+}
+
+myFuncThird("gkgkgkgk")
